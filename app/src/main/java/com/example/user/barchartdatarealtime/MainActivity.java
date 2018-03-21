8f2bea63ccab921c2764020ea172d14e3f4a17fd
TrungTin <trungtin.iuh>
2018-03-21 15:18:11 +0700

    final code
    update 21/03/2018

--- app/src/main/java/com/example/user/barchartdatarealtime/MainActivity.java
+++ app/src/main/java/com/example/user/barchartdatarealtime/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.user.barchartdatarealtime;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +27,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import de.tavendo.autobahn.WebSocket;
 import de.tavendo.autobahn.WebSocketConnection;
@@ -74,10 +77,23 @@ public class MainActivity extends AppCompatActivity {
         mWebSocketConnection = null;
     }
 
+    public static String getRandomColor() {
+        String[] letters = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+        String color = "#";
+        for (int i = 0; i < 6; i++ ) {
+            color += letters[(int) Math.round(Math.random() * 15)];
+        }
+        return color;
+    }
+
     BarChart barChart;
+    WebSocket.WebSocketConnectionObserver realTimeRetrieveHandle;
     private void connectToWebsocket(String wsURL) {
         String webSocketRealTimeDataUri = "wss://dataengine.globiots.com:443/data-engine/mobile/realtime";
-        final String messageTogetDataRealTime = "{\"objects\": [{\"addresses\": [{\"address\":\"3000\",\"dataType\":\"integer_16\",\"length\":2,\"value\":\"\"},{\"address\":\"3002\",\"dataType\":\"integer_16\",\"length\":2,\"value\":\"\"},{\"address\":\"3004\",\"dataType\":\"integer_16\",\"length\":2,\"value\":\"\"},{\"address\": \"201A\",\"dataType\": \"float\",\"length\": 4},{\"address\": \"2020\",\"dataType\": \"float\",\"length\": 4},{\"address\": \"2000\",\"dataType\": \"float\",\"length\": 4}],\"hostname\": \"0.0.0.254\"}],\"sessionId\": \"\",\"timezone\": \"GMT+07:00\",\"updateTime\": 3}";
+//        final String messageTogetDataRealTime = "{\"objects\": [{\"addresses\": [{\"address\":\"3000\",\"dataType\":\"integer_16\",\"length\":2,\"value\":\"\"},{\"address\":\"3002\",\"dataType\":\"integer_16\",\"length\":2,\"value\":\"\"},{\"address\":\"3004\",\"dataType\":\"integer_16\",\"length\":2,\"value\":\"\"},{\"address\": \"201A\",\"dataType\": \"float\",\"length\": 4},{\"address\": \"2020\",\"dataType\": \"float\",\"length\": 4},{\"address\": \"2000\",\"dataType\": \"float\",\"length\": 4}],\"hostname\": \"0.0.0.254\"}],\"sessionId\": \"\",\"timezone\": \"GMT+07:00\",\"updateTime\": 3}";
+
+        final String messageTogetDataRealTime = "{\"objects\": [{\"addresses\": [{\"address\":\"301A\",\"dataType\":\"integer_16\",\"length\":2,\"value\":\"\"},{\"address\":\"2300\",\"dataType\":\"integer_16\",\"length\":2,\"value\":\"\"},{\"address\":\"2004\",\"dataType\":\"integer_16\",\"length\":2,\"value\":\"\"},{\"address\": \"2065\",\"dataType\": \"float\",\"length\": 4},{\"address\": \"2304\",\"dataType\": \"float\",\"length\": 4}],\"hostname\": \"0.0.1.4\"}],\"sessionId\": \"\",\"timezone\": \"GMT+07:00\",\"updateTime\": 3}";
+
 
         // start init data - build layout
         barChart = new BarChart(getApplicationContext());
@@ -95,28 +111,28 @@ public class MainActivity extends AppCompatActivity {
         RealTimeStreamingData realTimeStreamingData = GsonUtil.getInstance()
                 .fromJson (messageTogetDataRealTime, RealTimeStreamingData.class);
 
-
+        List<IBarDataSet> iBarDataSetList = new ArrayList<>();
         for(ParameterObjectRealTimeData parameterObjectRealTimeData : realTimeStreamingData.getObjects()) {
 
-            List<IBarDataSet> iBarDataSetList = new ArrayList<>();
+
             for (AddressParameterRealTimeData addressParameterRealTimeData : parameterObjectRealTimeData.getAddresses()) {
 
                 String address = addressParameterRealTimeData.getAddress();
 
                 BarDataSet barDataSet = new BarDataSet(new ArrayList<BarEntry>(), address);
+                barDataSet.setColor(Color.parseColor(getRandomColor()));
                 iBarDataSetList.add(barDataSet);
 
                 startId++;
             }
 
-            BarData barData = new BarData(iBarDataSetList);
-
-            barChart.setData(barData);
 
         }
+        BarData barData = new BarData(iBarDataSetList);
 
+        barChart.setData(barData);
         try{
-            WebSocket.WebSocketConnectionObserver realTimeRetrieveHandle =
+            realTimeRetrieveHandle =
                     new WebSocket.WebSocketConnectionObserver() {
 
                         @Override
@@ -136,6 +152,7 @@ public class MainActivity extends AppCompatActivity {
                                     try {
                                         value = new Float(addressParameterRealTimeData.getValue());
                                     } catch (Exception e) {
+                                        value = 0f;
                                         Log.w("Error", e.toString());
                                     }
 
@@ -160,6 +177,9 @@ public class MainActivity extends AppCompatActivity {
 
                                         barData.setValueFormatter(new DefaultValueFormatter(2));
                                         barData.notifyDataChanged();
+                                        barChart.setBackgroundColor(Color.WHITE);
+                                        barChart.getXAxis().setEnabled(false);
+                                        barChart.getAxisRight().setEnabled(false);
                                         barChart.getAxisLeft().resetAxisMinimum();
                                         barChart.getAxisLeft().resetAxisMaximum();
                                         barChart.animateY(800);
